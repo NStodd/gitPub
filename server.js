234ced5fa6d4bf671b80a3b8e95c07ff782c9a9e
NateStodd <nathan.stoddard@gmail.com>
2022-11-14 19:25:30 -0500

    initial drinks route

--- server.js
+++ server.js
@@ -7,11 +7,18 @@ const app = express()
 // set PORT to 3000
 const PORT = 3000
 
+// connect to our drinks data
+const drinks = require('./models/drinks.js')
+
 // get route for the landing page
 app.get('/', (req, res) => {
     res.send("Welcome to the gitPub App!")
 })
 
+// get route for drinks
+app.get('/drinks', (req, res) => {
+    res.send(drinks)
+})
 
 // listen to port
 app.listen(PORT, () => {
